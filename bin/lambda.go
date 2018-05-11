7ebfd2855a1105751484a355fdce9cb89170ff4e
Josh Phillips <joshua.phillips@stelligent.com>
2018-05-11 13:14:01 -0400

    adding config service to handler

--- bin/lambda.go
+++ bin/lambda.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"errors"
 	"context"
+	"encoding/json"
+	"errors"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/configservice"
 	"log"
 	"strings"
-	"encoding/json"
 )
 
 func nameViolation(bucketName, region string) string {
@@ -19,16 +20,33 @@ func nameViolation(bucketName, region string) string {
 	}
 }
 
-func Handler(ctx context.Context, configEvent events.ConfigEvent ) string{
-	config := configservice.New(sess, &aws.Config)
-	invokingEvent :=
-	configurationItem :=
-	ruleParameters :=
-	violation := nameViolation(bucketName, region)
-	config := configservice.New(mySession)
-	config.
+func Handler(ctx context.Context, configEvent events.ConfigEvent) string {
+	// Creating session via
+	session := session.Must(session.NewSession())
+	config := configservice.New(session, &aws.Config{})
+	params := &configservice.PutEvaluationsInput{
+		ResultToken: aws.String("String"), // Required
+		Evaluations: []*configservice.Evaluation{
+			{ // Required
+				ComplianceResourceId:   aws.String("StringWithCharLimit256"), // Required
+				ComplianceResourceType: aws.String("StringWithCharLimit256"), // Required
+				ComplianceType:         aws.String("ComplianceType"),         // Required
+				OrderingTimestamp:      aws.Time(time.Now()),                 // Required
+				Annotation:             aws.String("StringWithCharLimit256"),
+			},
+			// More values...
+		},
+	}
+	resp, err = config.PutEvaluations(params)
 	evalution := evaluateCompliance(configurationItem, ruleParameters)
-	
+	if err != nil {
+		// Print the error, cast err to the awserr.Error to get the Code and
+		// Message from an error.
+		fmt.PrinLn(err.Error())
+		return
+	}
+	violation := nameViolation(bucketName, region)
+
 }
 
 func main() {
